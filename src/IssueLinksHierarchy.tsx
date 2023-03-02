4a6204c38fdc8b8244387d48fd8dd98d98b30498
RAHUL <rahulmeetawa@gmail.com>
2023-03-02 16:02:49 +0530

    update tsx

---
+++ src/IssueLinksHierarchy.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const IssueLinksHierarchy = () => {
+  return (
+    <h1>rahulsaini</h1>
+  );
+};
+
+export default IssueLinksHierarchy;
---
+++ src/IssueLinksHierarchyCloud.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IssueLinksHierarchy from './IssueLinksHierarchy';
+
+const app = document.getElementById('app');
+ReactDOM.render(
+  <IssueLinksHierarchy />,
+    app
+);
