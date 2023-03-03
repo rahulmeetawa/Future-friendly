1c5a8da3b3242a013b299cff6247d3f7260199b7
RAHUL <rahulmeetawa@gmail.com>
2023-03-03 18:59:21 +0530

    react folders names and its details

--- src/IssueLinksHierarchy.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const IssueLinksHierarchy = () => {
-  return (
-    <h1>rahulsaini</h1>
-  );
-};
-
-export default IssueLinksHierarchy;
--- src/IssueLinksHierarchyCloud.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IssueLinksHierarchy from './IssueLinksHierarchy';
-
-const app = document.getElementById('app');
-ReactDOM.render(
-  <IssueLinksHierarchy />,
-    app
-);
